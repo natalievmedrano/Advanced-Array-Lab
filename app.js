2c6161ac161767ee2f5f173ab715ef6ad3da7e07
natalievmedrano <nataliemed0403@gmail.com>
2023-01-20 14:00:24 -0600

    completed problem one

--- app.js
+++ app.js
@@ -140,11 +140,19 @@ console.log('mexicanFood from filterExample', mexicanFood)
 //Filter
 
 function problemOne(){
-
+    
     let results;
-
+    results = dishes.filter(function(el){
+        console.log("vegetarian dishes:",el)
+        if(el.cuisine === "Vegetarian"){
+            return true;
+        }})
+        
     return results;
 }
+
+let vegetarianFood =problemOne()
+console.log(vegetarianFood)
 
 //2. Create a function that will prompt the user to enter a cuisine type and then return all dishes that match that type
 //Filter
